7613d0030925360aaf7979823a557ba27e1c3960
naymmm <skeepy1000@gmail.com>
2024-11-17 21:18:24 +1100

    chore: add the acs stuff

---
+++ src/ServerStorage/Tools/M110/ACS_Modulo/Variaveis/Settings.lua
@@ -0,0 +1,150 @@
+local Tool = script.Parent.Parent.Parent
+local ACS = Tool:WaitForChild("ACS_Modulo")
+local Var = ACS:WaitForChild("Variaveis")
+
+local Config = {
+
+----------------------------------------------------------------------------------------------------
+---------------------=[ GERAL ]=--------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+	 Name = "M110"
+	,BulletType = "7.62×51mm NATO"
+	,Ammo = Var.Ammo.Value
+	,ChangeFOV = {35, 20}
+	,ZoomAnim = false
+	,FocusOnSight = true
+	,adsMesh1 = true
+	,FocusOnSight2 = true
+	,adsMesh2 = true
+	,MagCount = true
+	,Bullets = 1
+	,FireRate = 600
+	,BurstFireRate = 800
+	,BurstShot = 3
+	,GunType = 1				--- 1 - Rifle, 2 - Shotgun
+	,ReloadType = 1				--- 1 - Magazine, 2 - Shell inserting
+	,Mode = "Semi"				--- Semi, Burst, Auto, Explosive || Bolt-Action, Pump-Action
+	,FireModes = {
+		ChangeFiremode = false;		
+		Semi = true;
+		Burst = false;
+		Auto = true;
+		Explosive = false;}
+----------------------------------------------------------------------------------------------------
+-----------------------=[ PUMP-ACTION ]=------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+	,AutoChamber = false
+	,ChamberWhileAim = false
+----------------------------------------------------------------------------------------------------
+-----------------=[ RECOIL & PRECISAO ]=------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+	,VRecoil = {13,17}				--- Vertical Recoil
+	,HRecoil = {6,8}				--- Horizontal Recoil
+	,AimRecover = .75				---- Between 0 & 1
+	
+	,RecoilPunch = .5
+	,VPunchBase = 2.5				--- Vertical Punch
+	,HPunchBase = 1.7				--- Horizontal Punch
+	,DPunchBase = 1 				--- Tilt Punch | useless
+	
+	,AimRecoilReduction = 5 		--- Recoil Reduction Factor While Aiming (Do not set to 0)
+	,PunchRecover = 0.2
+	,MinRecoilPower = 2
+	,MaxRecoilPower = 3
+	,RecoilPowerStepAmount = .25
+	
+	,MinSpread = 0				--- Min bullet spread value | Studs
+	,MaxSpread = 0				--- Max bullet spread value | Studs
+	,AimInaccuracyStepAmount = 0
+	,WalkMultiplier = 0				--- Bullet spread based on player speed
+
+	,SwayBase = 0.15				--- Weapon Base Sway | Studs
+	,MaxSway =	1					--- Max sway value based on player stamina | Studs
+----------------------------------------------------------------------------------------------------
+---------------------=[ DANO ]=---------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+	,LimbsDamage = {35,45}
+	,TorsoDamage = {45,55}
+	,HeadDamage  = {105,115}
+	,BulletPenetration = 75			 ---- Between 0 & 100%
+	,FallOfDamage = 0
+----------------------------------------------------------------------------------------------------
+-------------------=[ PROJETIL ]=-------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+	,Distance = 10000
+	,BDrop = .03
+	,BSpeed = 650
+
+	,SuppressMaxDistance = 25	--- Studs
+	,SuppressTime = 10			--- Seconds
+
+	,BulletWhiz = true
+	,BWEmitter = 25
+	,BWMaxDistance = 200
+	
+	,BulletFlare = false
+	,BulletFlareColor = Color3.fromRGB(255,255,255)
+
+	,Tracer = true
+	,TracerColor = Color3.fromRGB(255, 75, 75)
+	,RandomTracer = false
+	,TracerChance = 100				-- only applies if above is true
+	,TracerEveryXShots = 3
+	,TracerBrightness = 2			-- min of 1, max of 4
+	
+	,BulletLight = true
+	,BulletLightBrightness = 0.35
+	,BulletLightColor = Color3.fromRGB(255, 138, 138)
+	,BulletLightRange = 10
+
+	,ExplosiveHit = false
+	,ExPressure = 500
+	,ExpRadius = 25
+	,DestroyJointRadiusPercent = 0	--- Between 0 & 1
+	,ExplosionDamage = 100
+
+	,LauncherDamage = 100
+	,LauncherRadius = 25
+	,LauncherPressure = 500
+	,LauncherDestroyJointRadiusPercent = 0
+----------------------------------------------------------------------------------------------------
+--------------------=[ OUTROS ]=--------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+	,FastReload = true					--- Automatically operates the bolt on reload if needed
+	,SlideLock = true
+	,MoveBolt = false
+	,BoltLock = false
+	,CanBreachDoor = false
+	,CanBreak = false					--- Weapon can jam?
+	,JamChance = 0						--- This old piece of brick doesn't work fine >;c
+	,IncludeChamberedBullet = true		--- Include the chambered bullet on next reload
+	,Chambered = false					--- Start with the gun chambered?
+	,LauncherReady = false				--- Start with the GL ready?
+	,CanCheckMag = true					--- You can check the magazine
+	,ArcadeMode = false					--- You can see the bullets left in magazine
+	,RainbowMode = false				--- Operation: Party Time xD
+	,ModoTreino = false					--- Surrender enemies instead of killing them
+	,GunSize = 4.5
+	,GunFOVReduction = 5
+	,fakeArms = false
+	,BoltExtend = Vector3.new(0, 0, 0.4)
+	,SlideExtend = Vector3.new(0, 0, 0.4)	
+----------------------------------------------------------------------------------------------------
+--------------------=[ CFRAME ]=--------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+	,EnableHolster = false
+	,HolsterTo = 'Torso'				 -- Put the name of the body part you wanna holster to
+	,HolsterPos = CFrame.new(0.25,-0.1,-0.8) * CFrame.Angles(math.rad(5),math.rad(-5),math.rad(50))
+	
+	,RightArmPos = CFrame.new(-0.85, -0.2, -1.2) * CFrame.Angles(math.rad(-90), math.rad(0), math.rad(0))	--Server
+	,LeftArmPos = CFrame.new(1.05,0.9,-1.4) * CFrame.Angles(math.rad(-100),math.rad(25),math.rad(-20))	--server
+	
+	,ServerGunPos = CFrame.new(-.3, -1, -0.4) * CFrame.Angles(math.rad(-90), math.rad(0), math.rad(0))
+	,GunPos = CFrame.new(0.15, -0.15, 1) * CFrame.Angles(math.rad(90), math.rad(0), math.rad(0))
+
+	,RightPos = CFrame.new(-.65, -0.2, -1) * CFrame.Angles(math.rad(-90), math.rad(0), math.rad(0))	--Client
+	,LeftPos = CFrame.new(1.2,0.1,-1.6) * CFrame.Angles(math.rad(-120),math.rad(35),math.rad(-20))	--Client
+}
+
+return Config
+
